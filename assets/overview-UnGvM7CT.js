80606810b13c7c27e94f4b05850d71498fb92d78
unional <unional@users.noreply.github.com>
2026-01-25 09:51:59 +0000

    deploy: f3430571f0e179577f6c13c3c39613d0338a5459

---
+++ assets/overview-UnGvM7CT.js
@@ -0,0 +1,6 @@
+import{j as e}from"./jsx-runtime-u17CrQMm.js";import{useMDXComponents as i}from"./index-DeKKlNtc.js";import{M as r}from"./blocks-Z056Vd7-.js";import"./iframe-DagrF6pB.js";import"./preload-helper-PPVm8Dsz.js";import"./index-CauS-PLC.js";function o(n){const t={code:"code",h1:"h1",p:"p",...i(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{title:"Overview"}),`
+`,e.jsx(t.h1,{id:"overview",children:"Overview"}),`
+`,e.jsx(t.p,{children:"This is a template for nodejs library."}),`
+`,e.jsx(t.p,{children:"Feel free to delete it if you don't need it."}),`
+`,e.jsxs(t.p,{children:[`If you want to use it,
+update the `,e.jsx(t.code,{children:"package.json"})," and this ",e.jsx(t.code,{children:"README.md"})," file."]})]})}function l(n={}){const{wrapper:t}={...i(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o(n)}export{l as default};
